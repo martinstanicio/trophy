fbe7d9faa02f079ad3f8696760cfd00bacf47a4b
Martín Stanicio <martuytimo@gmail.com>
2023-12-29 19:06:05 -0300

    highlight current route in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,24 @@
-import { PiggyBank, QrCode, Trophy } from "lucide-react";
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import routes from "@/lib/routes";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
-  const links = [
-    { label: "Prizes", href: "/prizes", icon: Trophy },
-    { label: "QR Code", href: "/", icon: QrCode },
-    { label: "Points", href: "/points", icon: PiggyBank },
-  ];
+  const pathname = usePathname();
 
   return (
     <nav className="sticky bottom-0 flex">
-      {links.map(({ label, href, icon: Icon }, i) => (
+      {routes.map(({ label, href, icon: Icon }, i) => (
         <a
           href={href}
           title={label}
           key={i}
-          className="flex-1 border-t-4 border-current bg-primary-foreground p-4 text-primary transition-colors"
+          className={cn(
+            "flex-1 border-t-4 border-current bg-primary-foreground p-4 text-primary transition-colors",
+            pathname !== href && "opacity-40 grayscale",
+          )}
         >
           <Icon className="mx-auto" size="1.5rem" />
           <span className="sr-only">{label}</span>
---
+++ src/lib/routes.ts
@@ -0,0 +1,9 @@
+import { PiggyBank, QrCode, Trophy } from "lucide-react";
+
+const routes = [
+  { label: "Prizes", href: "/prizes", icon: Trophy },
+  { label: "QR Code", href: "/", icon: QrCode },
+  { label: "Points", href: "/points", icon: PiggyBank },
+];
+
+export default routes;
